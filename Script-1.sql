64f10d35cc32da2bf688fb06ca80c3f27ec5ded4
AlenChet <115559312+AlenChet@users.noreply.github.com>
2023-04-29 14:15:02 +0300

    Update Script
    Добавлено условие, если поле будет пустым

--- Script-1.sql
+++ Script-1.sql
@@ -2,5 +2,5 @@ create table if not exists Employee (
 	employee_id SERIAL primary key,
 	name VARCHAR(120) not null,
 	departament VARCHAR(120),
-	boss_id integer references Employee(employee_id)
+	boss_id integer references Employee(employee_id) ON DELETE SET NULL 
 );
